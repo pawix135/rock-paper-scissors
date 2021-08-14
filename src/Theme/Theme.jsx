f407cb8871ec7198b06c9f9081de6ccf6694bf41
pawix135 <leagueoflegendsoppl@gmail.com>
2021-08-14 16:57:17 +0200

    1st.

---
+++ src/Theme/Theme.jsx
@@ -0,0 +1,14 @@
+import { theme } from './styles'
+import { ThemeProvider } from "styled-components"
+
+
+
+const Theme = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      
+    </ThemeProvider>
+  )
+}
+
+export default Theme
---
+++ src/Theme/styles.js
@@ -0,0 +1,11 @@
+
+export const theme = {
+
+  body: {
+    background: "radial-gradient(at top, 	#1f3756, #141539)"
+  },
+  scissors: {
+    
+  }
+  
+}
